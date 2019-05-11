5b528f5b538389b27ad5bb5304f7c7fb23daf1aa
conroy12345 <35980602+conroy12345@users.noreply.github.com>
2019-05-11 06:31:41 +0100

    Create Constants.java

---
+++ src/main/java/Constants.java
@@ -0,0 +1,11 @@
+package org.reader.app;
+
+import java.util.Scanner;
+
+public class Constants {
+
+	static Scanner SCANNER_INPUT = new Scanner(System.in);
+	static String SECRET_TORD = "SECRET WORD";
+	static String GUESSES = "";
+	static int NUMBER_OG_SEARCHES = 0;
+}
